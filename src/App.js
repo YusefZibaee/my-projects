5433a4055d3d05f105a02a5fa0b7a9a4b4d4cac6
yusefzibaee <yusefzibaee@gmail.com>
2025-01-26 11:13:15 +0330

    Remove my-projects from git index

--- src/App.js
+++ src/App.js
@@ -1,20 +1,88 @@
-import React from 'react'
-import routes from './routses'
-import {useRoutes} from 'react-router-dom'
-import TopBar from './component/TopBar/TopBar'  
-import Saidebar from './component/sidebar/Saidebar' 
+import React, { useEffect, useState } from "react";
 
-import './App.css' 
 export default function App() {
-  let router = useRoutes(routes)
-  return (
-    <>
-    <TopBar/>
-    <div className="container">
-    {router}
-    <Saidebar/>
-    </div>
-   </>
-  )
-}
+  const [todos, setTodos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [paginatedTodos, setPaginatedTodos] = useState([]);
+  let pagSize = 10;
+  let pageNumber;
+  const changePaginate = (newPage) => {
+    setCurrentPage(newPage);
+  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+        let endIndex = pagSize * currentPage;
 
+        let startIndex = endIndex - pagSize;
+        let allShownTodos = data.slice(startIndex, endIndex);
+        setPaginatedTodos(allShownTodos);
+      });
+  }, []);
+
+  useEffect(() => {
+    let endIndex = pagSize * currentPage;
+    let startIndex = endIndex - pagSize;
+
+    let allShownTodos = todos.slice(startIndex, endIndex);
+    setPaginatedTodos(allShownTodos);
+  }, [currentPage]);
+
+  const pageCount = Math.ceil(todos.length / pagSize);
+  pageNumber = Array.from(Array(pageCount).keys());
+  return (
+    <div>
+      {!todos ? (
+        "loading"
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USER ID</th>
+              <th>TITLE</th>
+              <th>STATUS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {paginatedTodos.map((todo) => (
+              <tr>
+                <td>{todo.id}</td>
+                <td>{todo.userId}</td>
+                <td>{todo.title}</td>
+                <td>
+                  <p
+                    className={
+                      todo.completed ? "btn btn-success" : "btn btn-danger"
+                    }
+                  >
+                    {todo.completed ? "completed" : "pending"}
+                  </p>
+                </td>
+                {console.log(todo)}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
+      <nav className="d-flex justify-content-center">
+        <ul class="pagination " aria-current="page">
+          {pageNumber.map((pageNum) => (
+            <li
+              class={
+                pageNum + 1 === currentPage ? "page-item active" : "page-item"
+              }
+              aria-current="page"
+              onClick={() => changePaginate(pageNum + 1)}
+            >
+              <span class="page-link">{pageNum + 1}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
